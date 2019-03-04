1080376d3331face48a094b71e415fd1503f804f
hash99 <39696599+hash99@users.noreply.github.com>
2019-03-04 19:38:13 +0100

    Update xxMieleathome.js

--- test/testscript/xxMieleathome.js
+++ test/testscript/xxMieleathome.js
@@ -1,12 +1,4 @@
-var Mieleathome = require('../../utils/mieleathome');
-var Miele_account='helmut.horras@iesy.net';
-var Miele_pwd='Sigrid-99';
-var client_id='d96bb945-2cd2-4a6c-a711-4e0fbf1ff9d1'
-var client_secret='usw6bg0n2f6scg8fvcgmgwbiuexj7kef'
-var refresh_token='DE_8e99bbb7d8712fad862ff2b729240191';
-var access_token='DE_46b9e88e282d5162a3e1c8f09b9126c6';
-var devices='000149287573';
-var miele = new Mieleathome(Miele_account,Miele_pwd,client_id,client_secret);
+ome(Miele_account,Miele_pwd,client_id,client_secret);
 miele.log("Test Exports");
 //miele.GetToken(Miele_pwd, Miele_account,client_id,client_secret,function(err,access_token,refresh_token){
 //console.log(err);
