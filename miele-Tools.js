b4060cb138dd766e3d1b31655d5a81debc62ef60
grizzelbee <hanjo@hingsen.de>
2021-05-31 13:28:19 +0200

    fixed >missing object< bug on arrays

--- miele-Tools.js
+++ miele-Tools.js
@@ -738,14 +738,7 @@ module.exports.createArray = function(adapter, setup, path, description, value){
         if (items > 1){
             MyPath = path + '_' + n;
         }
-        if (value[n].value_localized !== null){
-            adapter.log.debug('createArray: Path:['   + MyPath  + ']');
-            adapter.log.debug('createArray:  value:[' + JSON.stringify(value)   + ']');
-            adapter.log.debug('createArray:  OrgUnit: [' + value[n].unit + ']');
-            mieleTools.createNumber(adapter, setup, MyPath, description, Number.parseInt(value[n].value_localized), value[n].unit, 'value.temperature')
-        } else {
-            adapter.log.debug('createArray: Invalid values detected. Skipped creating this data point.');
-        }
+        mieleTools.createNumber(adapter, setup, MyPath, description, Number.parseInt(value[n].value_localized), value[n].unit, 'value.temperature')
     }
 }
 
